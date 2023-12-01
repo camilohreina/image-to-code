9a5916dcd087c9e475a9e88ca8227212d1969d87
Camilo Hernández <camilohreina.dev@gmail.com>
2023-11-30 19:39:46 -0500

    create form and styles website

---
+++ app/form.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export const Form = () => {
+
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        const form = event.currentTarget
+        const url = form.elements.namedItem('url') as HTMLInputElement
+
+        console.log(url.value)
+    }
+
+    return (
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <Label htmlFor="url">URL of image</Label>
+            <Input id="url" type="url" placeholder="https://something.com/avatar.png" />
+            <Button>Generate code from image</Button>
+        </form>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { Form } from './form'
 
 export default function Home() {
   return (
-    <main className="">
-      Hola mundo
-    </main>
+    <div className="grid grid-cols-[400px_1fr]">
+      <aside className='flex flex-col justify-between min-h-screen p-4 bg-gray-900 '>
+        <header className='text-center'>
+          <h1 className='text-3xl font-semibold'>Image 2 Code</h1>
+          <h2 className='text-sm opacity-75'>Pass one image to code in seconds...</h2>
+        </header>
+
+        <section>
+          {/* Filters will be here */}
+        </section>
+
+        <footer>
+          Desarrollado por xDiffrent
+        </footer>
+      </aside>
+      <main className='bg-gray-950'>
+        <section className='max-w-2xl mx-auto p-10'>
+          <Form />
+        </section>
+      </main>
+    </div>
   )
 }
