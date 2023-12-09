ff75e865099b48796e13499a8a3280af5b028485
Camilo Hernández <camilohreina.dev@gmail.com>
2023-12-09 18:20:02 -0500

    Add FormChat component

--- app/code-preview.tsx
+++ app/code-preview.tsx
@@ -4,6 +4,7 @@ import { DisplayCode } from "./display-code";
 import { CodeReview } from "./code-review";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import { FormChat } from "./form-chat";
 
 export const CodePreview = ({ background, html }: { background: string, html: string }) => {
     return (
@@ -20,16 +21,7 @@ export const CodePreview = ({ background, html }: { background: string, html: st
                     <CodeReview html={html} />
                 </TabsContent>
             </Tabs>
-            <form className="overflow-hidden [&amp;:has(textarea:focus)]:border-token-border-xheavy [&amp;:has(textarea:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)] flex flex-col w-full dark:border-token-border-heavy flex-grow relative border border-token-border-heavy dark:text-white rounded-2xl bg-white dark:bg-gray-800 shadow-[0_0_0_2px_rgba(255,255,255,0.95)] dark:shadow-[0_0_0_2px_rgba(52,53,65,0.95)]">
-                <Textarea id="prompt-textarea" data-id="root" rows={1} placeholder="Typing here..." className="m-0 w-full min-h-[50px] resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-3.5 md:pr-12 placeholder-black/50 dark:placeholder-white/50 pl-3 md:pl-4" style={{ maxHeight: '200px', height: '52px', overflowY: 'hidden' }} />
-                <Button className="max-w-[32px] max-h-[32px] absolute md:bottom-3 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 dark:disabled:bg-white disabled:bg-black disabled:opacity-10 disabled:text-gray-400 enabled:bg-black text-white p-0.5 border border-black rounded-lg dark:border-white dark:bg-white bottom-1.5 transition-colors" data-testid="send-button">
-                    <span className="" data-state="closed">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" className="text-white dark:text-black">
-                            <path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </span>
-                </Button>
-            </form>
+            <FormChat />
         </div>
     )
 
